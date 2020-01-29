6bec1ed6c9462514b695a8ed1a6c173b3064618d
Rodrigo Dematei de freitas <rodfrei@gmail.com>
2020-01-29 03:48:39 +0000

    Teste de Rota

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+const router = express.Router();
 
 app.get('/', (req, res) => {
-    res.send('Hello')
-
+res.render('index', function (err, html) {
+  res.send(html)
+})
+    
 });
 
 app.listen(3001);
